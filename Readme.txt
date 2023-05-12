5714e832fe4cfc4953d718206d9c9fdb17396b14
vsuha <vsuhas92@gmail.com>
2023-05-11 19:37:39 -0600

    - Added instructions to update teams and skill levels if necessary.

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,9 @@
+APA POOL STRATEGY
+
 This script will help to build a strategy of possible combinations of players for APA Pool team
-1. Run the script
-2. Press 8 or 9 to select pool format from the main menu
-3. Press number to select player
-4. Press q to save and return to main menu
-5. Press r to change previously selected player
+1. Update Team and skill level of the players in team_9ball and team_8ball 
+2. Run the script
+3. Press 8 or 9 to select pool format from the main menu
+4. Press number to select player
+5. Press q to save and return to main menu
+6. Press r to change previously selected player
--- pool_statergy_APA.py
+++ pool_statergy_APA.py
@@ -1,17 +1,19 @@
 """
 This script will help to build a strategy of possible combinations of players for APA Pool team
-1. Run the script
-2. Press 8 or 9 to select pool format from the main menu
-3. Press number to select player
-4. Press q to save and return to main menu
-5. Press r to change previously selected player
-
+1. Update Team and skill level of the players in team_9ball and team_8ball
+2. Run the script
+3. Press 8 or 9 to select pool format from the main menu
+4. Press number to select player
+5. Press q to save and return to main menu
+6. Press r to change previously selected player
 """
 
 import numpy as np
 import os
 from colorama import Fore
 
+team_9ball = {'Howard': 7, 'Bobby': 6, 'Renee': 5, 'Kat': 4, 'Suhas': 4, 'Ellen': 3, 'Pam': 2, 'Meghan': 1}
+team_8ball = {'Howard': 6, 'Bobby': 6, 'Renee': 5, 'Kat': 4, 'Suhas': 4, 'Ellen': 4, 'Pam': 3, 'Meghan': 2}
 
 def clear_console():
     if os.name == 'nt':  # for Windows
@@ -191,9 +193,6 @@ def played(played):
                 print(' -- ', end='')
         print('\n')
 
-
-team_9ball = {'Howard': 7, 'Bobby': 6, 'Renee': 5, 'Kat': 4, 'Suhas': 4, 'Ellen': 3, 'Pam': 2, 'Meghan': 1}
-team_8ball = {'Howard': 6, 'Bobby': 6, 'Renee': 5, 'Kat': 4, 'Suhas': 4, 'Ellen': 4, 'Pam': 3, 'Meghan': 2}
 
 # Run all possible options with skill levels and max points
 result_9 = allCombinations(team=team_9ball, maxpoints=23)
